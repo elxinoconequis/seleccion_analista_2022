913545a05967c7f071d774efed0e4f95c9c13976
Joaquín Fernando Ortega Silva <jf.ortega.silva@gmail.com>
2022-02-14 10:52:58 -0600

    Black format a dummy_model.py
    Se aplico el formato Black, de manera que espació algunas asignaciones y funciones.

--- pollos_petrel/dummy_model.py
+++ pollos_petrel/dummy_model.py
@@ -8,7 +8,7 @@ def read_training_dataset() -> pd.DataFrame:
     submission_file_name = "JFOS_submission.csv"
     training_dataset_path = "pollos_petrel/train.csv"
     training_dataset = pd.read_csv(training_dataset_path)
-    print("Length: ",len(training_dataset),type(training_dataset))
+    print("Length: ", len(training_dataset), type(training_dataset))
     print(training_dataset.head())
 
     return training_dataset
@@ -24,7 +24,7 @@ def get_target_mean(dataset: pd.DataFrame) -> float:
 def read_testing_dataset() -> pd.DataFrame:
     testing_dataset_path = "pollos_petrel/test.csv"
     testing_dataset = pd.read_csv(testing_dataset_path)
-    print("Length: ",len(testing_dataset),type(testing_dataset))
+    print("Length: ", len(testing_dataset), type(testing_dataset))
     print(testing_dataset.head())
     return testing_dataset
 
@@ -47,43 +47,44 @@ def add_mean_as_target() -> pd.DataFrame:
 
 # Guarda el archivo con sufijo _submission.csv
 def write_submission():
-    filename="JFOS_submission.csv"
-    submission_path = "pollos_petrel/"+filename
+    filename = "JFOS_submission.csv"
+    submission_path = "pollos_petrel/" + filename
     submission = add_mean_as_target()
     submission.to_csv(submission_path)
+
 
 def resumen_5_num(dataset: pd.DataFrame) -> pd.DataFrame:
     # Sample minimum
 
+    return
 
-    return 
 
 ## ------MAIN-----------------------
-cwd=os.getcwd()
-print("\nCurrent Working Directory",cwd)
+cwd = os.getcwd()
+print("\nCurrent Working Directory", cwd)
 print("List files and directories: \n", os.listdir())
 print("Pandas version: ", pd.__version__)
 
 
 # 1.- Ajusta un modelo con el archivo train.csv
 print("\nLeer /train.csv")
-df_train=read_training_dataset()
+df_train = read_training_dataset()
 
-df_train=df_train.fillna(0)
-print(df_train[["Masa","target"]])
+df_train = df_train.fillna(0)
+print(df_train[["Masa", "target"]])
 
 sort_by_mass = df_train.sort_values(by=["Masa"])
 print(sort_by_mass)
 
-sort_by_day=df_train.sort_values(by=["target"])
+sort_by_day = df_train.sort_values(by=["target"])
 print(sort_by_day)
 
 # 5-Num summary
-print("5-Num Summary\n",sort_by_day.describe())
+print("5-Num Summary\n", sort_by_day.describe())
 
 # 2.- Evalúa el modelo ajustado en test.csv
-#print("\nLeer /test.csv")
-#df_test=read_testing_dataset()
+# print("\nLeer /test.csv")
+# df_test=read_testing_dataset()
 
 
 # 3.- Guarda la respuesta en un archivo .csv
@@ -91,4 +92,3 @@ submission_file_name = "JFOS_submission.csv"
 
 # Resultado: Estimar la edad de acuerdo a la morfometría
 # id,edad estimada
-
