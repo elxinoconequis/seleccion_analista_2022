c5fd86d9e87a6141959e224bd8faa7f353d85053
Joaquín Fernando Ortega Silva <jf.ortega.silva@gmail.com>
2022-03-08 21:04:54 -0800

    :art: Mejorar estructura del código, dividir en varias funciones
    Nuevos test, para nuevas funciones
    
    :space_invader: eliminados

--- pollos_petrel/make_diagram.py
+++ pollos_petrel/make_diagram.py
@@ -1,27 +1,50 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from dummy_model import (
     clean_NA,
     read_training_dataset,
 )
 
 
-def make_box_diagram():
-    clean_df = clean_NA(read_training_dataset(), 0)
-    # Creating figure w/ two subplots
+def make_box_diagram(clean_df: pd.DataFrame):
     fig, axs = plt.subplots(1, 2)
-    sub_plot_1 = axs[0].boxplot(clean_df["target"])
-    axs[0].set_title("Días")
+    plot_first_subplot(clean_df, axs, 0)
+    plot_second_subplot(clean_df, axs, 1)
+    save_figure()
+    return axs
+
+
+# Hacer primer subplot
+def plot_first_subplot(clean_df: pd.DataFrame, axs: np.array, index: int):
+    sub_plot_1 = axs[index].boxplot(clean_df["target"])
+    axs[index].set_title("Días")
     plt.setp(sub_plot_1["boxes"], color="red")
     plt.setp(sub_plot_1["whiskers"], color="red")
-    axs[0].yaxis.grid(True, linestyle="-", which="major", color="lightgrey", alpha=0.5)
+    make_grid_first_subplot(axs, index)
 
-    sub_plot_2 = axs[1].boxplot(clean_df["Masa"])
-    axs[1].set_title("Masa")
+
+# Hacer segundo subplot
+def plot_second_subplot(clean_df: pd.DataFrame, axs: np.array, index: int):
+    sub_plot_2 = axs[index].boxplot(clean_df["Masa"])
+    axs[index].set_title("Masa")
     plt.setp(sub_plot_2["boxes"], color="blue")
     plt.setp(sub_plot_2["whiskers"], color="blue")
-    axs[1].yaxis.grid(True, linestyle="-", which="major", color="lightgrey", alpha=0.5)
+    make_grid_second_subplot(axs, index)
 
+
+# Hacer lineas grid de un subplot color gris tenue para primer subplot
+def make_grid_first_subplot(axs: np.array, index: int):
+    axs[index].yaxis.grid(
+        linestyle="-", which="major", color="lightgrey", alpha=0.5
+    )  # Si hay otros argumentos, la función asume un True
+
+
+# Hacer lineas grid de un subplot color gris tenue para segundo subplot
+def make_grid_second_subplot(axs: np.array, index: int):
+    axs[index].yaxis.grid(linestyle="-", which="major", color="lightgrey", alpha=0.5)
+
+
+# Guardar figura
+def save_figure():
     plt.savefig("images/Box_diagram_train.png")
-
-    return axs
--- tests/test_make_diagram.py
+++ tests/test_make_diagram.py
@@ -16,13 +16,13 @@ def test_make_box_diagram_path():
     submission_path = "images/Box_diagram_train.png"
     if os.path.exists(submission_path):
         os.remove(submission_path)
-    make_box_diagram()
+    make_box_diagram(clean_NA(read_training_dataset(), 0))
     assert os.path.exists(submission_path)
     os.remove(submission_path)
     assert submission_path is not None
 
 
-# Test to check the clean_NA() function within make_diagram.py for train.csv
+# Test to check the clean_NA() function within make_diagram.py for train.csv - revisar
 def test_make_box_diagram_clean_df():
     obtained_dataset = read_training_dataset()
     obtained_val_index_0_6 = obtained_dataset.iloc[0][6]
@@ -30,9 +30,27 @@ def test_make_box_diagram_clean_df():
     assert is_nan_val_index_0_6
 
 
-# Test para cehcar dimensiones de la variable axs
+# Test para checar dimensiones de la variable axs, para train.csv
 def test_size_subplot_axs():
     expected_size_axs = np.size(np.array([1, 2]))
-    axs = make_box_diagram()
+    axs = make_box_diagram(clean_NA(read_training_dataset(), 0))
     obtained_size_axs = np.size(axs)
     assert obtained_size_axs == expected_size_axs
+
+
+# Test para el primer subplot
+def test_plot_first_subplot():
+    clean_df = clean_NA(read_training_dataset(), 0)
+    obtained_axs = make_box_diagram(clean_df)  # from matplotlib
+    obtained_axs_name = obtained_axs[0].get_title(loc="center")
+    expected_axs_name = "Días"
+    assert obtained_axs_name == expected_axs_name
+
+
+# Test para el segundo subplot
+def test_plot_second_subplot():
+    clean_df = clean_NA(read_training_dataset(), 0)
+    obtained_axs = make_box_diagram(clean_df)  # from matplotlib
+    obtained_axs_name = obtained_axs[1].get_title(loc="center")
+    expected_axs_name = "Masa"
+    assert obtained_axs_name == expected_axs_name
